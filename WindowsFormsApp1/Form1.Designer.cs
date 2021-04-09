897fa867fd6f7aac86ff82bba6c699ff802c6001
PanzerNyan <Kwii.kaiser@gmail.com>
2021-04-09 11:35:36 +0300

    commit 1

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Encryptbutton = new System.Windows.Forms.Button();
+            this.Decryptbutton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 53);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(269, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Encryptbutton
+            // 
+            this.Encryptbutton.Location = new System.Drawing.Point(11, 122);
+            this.Encryptbutton.Name = "Encryptbutton";
+            this.Encryptbutton.Size = new System.Drawing.Size(267, 26);
+            this.Encryptbutton.TabIndex = 1;
+            this.Encryptbutton.Text = "Encrypt";
+            this.Encryptbutton.UseVisualStyleBackColor = true;
+            this.Encryptbutton.Click += new System.EventHandler(this.Encryptbutton_Click);
+            // 
+            // Decryptbutton
+            // 
+            this.Decryptbutton.Location = new System.Drawing.Point(13, 154);
+            this.Decryptbutton.Name = "Decryptbutton";
+            this.Decryptbutton.Size = new System.Drawing.Size(267, 26);
+            this.Decryptbutton.TabIndex = 2;
+            this.Decryptbutton.Text = "Decrypt";
+            this.Decryptbutton.UseVisualStyleBackColor = true;
+            this.Decryptbutton.Click += new System.EventHandler(this.Decryptbutton_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(11, 88);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(269, 27);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(5F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(291, 203);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Decryptbutton);
+            this.Controls.Add(this.Encryptbutton);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Futura LtCn BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Encryptbutton;
+        private System.Windows.Forms.Button Decryptbutton;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Xml;
+using System.Xml.Serialization;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        RSAParameters privateKey;
+        RSAParameters publicKey;
+        RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+        public Form1()
+        {
+            InitializeComponent();
+
+            //RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(); 
+
+            privateKey = rsa.ExportParameters(true);
+            publicKey = rsa.ExportParameters(false);
+        }
+
+        private void Encryptbutton_Click(object sender, EventArgs e)
+        {
+            UnicodeEncoding byteConverter = new UnicodeEncoding();
+            byte[] input = byteConverter.GetBytes(textBox1.Text);
+            string output;
+
+            output = RSAEncryption(textBox1.Text, publicKey);
+
+            MessageBox.Show(output, "Encryption");
+            string path = @"E:\Visual_Studio_Projects\Information_security_03\result.txt";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine (output);
+                }
+            }        
+            
+            GetPublicKeyString();
+            textBox2.Text = output;
+        }
+
+        private void Decryptbutton_Click(object sender, EventArgs e)
+        {
+          
+
+            UnicodeEncoding byteConverter = new UnicodeEncoding();
+            //byte[] input = byteConverter.GetBytes(textBox2.Text);
+            string output;
+
+            output = RSADecryption(textBox2.Text, privateKey);
+
+            MessageBox.Show(output, "Decryption");
+        }
+
+        static public string RSAEncryption(string dataToEncrypt, RSAParameters publicKeyInfo)
+        {
+            byte[] encryptedBytes = new byte[0];
+            using (var RSA = new RSACryptoServiceProvider())
+            {
+                try
+                {
+                    UTF8Encoding encoder = new UTF8Encoding();
+
+                    byte[] encryptThis = encoder.GetBytes(dataToEncrypt);
+
+                    RSA.ImportParameters(publicKeyInfo);
+
+                    int blockSize = (RSA.KeySize / 8) - 32;
+                    byte[] buffer = new byte[blockSize];
+                    byte[] encryptedBuffer = new byte[blockSize];
+                    encryptedBytes = new byte[encryptThis.Length + blockSize - (encryptThis.Length % blockSize) + 32];
+
+                    for (int i = 0; i < encryptThis.Length; i += blockSize)
+                    {
+                        if (2 * i > encryptThis.Length && ((encryptThis.Length - i) % blockSize != 0))
+                        {
+                            buffer = new byte[encryptThis.Length - i];
+                            blockSize = encryptThis.Length - i;
+                        }
+
+                        if (encryptThis.Length < blockSize)
+                        {
+                            buffer = new byte[encryptThis.Length];
+                            blockSize = encryptThis.Length;
+                        }
+
+                        Buffer.BlockCopy(encryptThis, i, buffer, 0, blockSize);
+                        encryptedBuffer = RSA.Encrypt(buffer, false);
+                        encryptedBuffer.CopyTo(encryptedBytes, i);
+                    }
+                }
+                catch (CryptographicException ex)
+                {
+                    Console.Write(ex);
+                }
+                finally
+                {
+                    RSA.PersistKeyInCsp = false;
+                }
+            }
+            return Convert.ToBase64String(encryptedBytes);
+            /*RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+            RSA.ImportParameters(RSAKeyInfo);
+            return RSA.Encrypt(DataToEncrypt, DoOAEPPadding);*/
+        }
+
+        static public string RSADecryption(string dataToDecrypt, RSAParameters privateKeyInfo)
+        {
+            byte[] decryptedBytes;
+
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                try
+                {
+                    byte[] bytesToDecrypt = Convert.FromBase64String(dataToDecrypt);
+
+                    RSA.ImportParameters(privateKeyInfo);
+
+                    int blockSize = RSA.KeySize / 8;
+
+                    byte[] buffer = new byte[blockSize];
+
+                    byte[] decryptedBuffer = new byte[blockSize];
+
+                    decryptedBytes = new byte[dataToDecrypt.Length];
+
+                    for (int i = 0; i < bytesToDecrypt.Length; i += blockSize)
+                    {
+                        if (2 * i > bytesToDecrypt.Length && ((bytesToDecrypt.Length - i) % blockSize != 0))
+                        {
+                            buffer = new byte[bytesToDecrypt.Length - i];
+                            blockSize = bytesToDecrypt.Length - i;
+                        }
+
+                        if (bytesToDecrypt.Length < blockSize)
+                        {
+                            buffer = new byte[bytesToDecrypt.Length];
+                            blockSize = bytesToDecrypt.Length;
+                        }
+
+                        Buffer.BlockCopy(bytesToDecrypt, i, buffer, 0, blockSize);
+                        decryptedBuffer = RSA.Decrypt(buffer, false);
+                        decryptedBuffer.CopyTo(decryptedBytes, i);
+                    }
+                }
+                finally
+                {
+                    RSA.PersistKeyInCsp = false;
+                }
+            }
+
+            var encoder = new UTF8Encoding();
+            return encoder.GetString(decryptedBytes).TrimEnd(new[] { '\0' });
+            //return Convert.ToString(decryptedBytes);
+
+            /*RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+            RSA.ImportParameters(RSAKeyInfo);
+            return RSA.Decrypt(DataToDecrypt, DoOAEPPadding);*/
+        }
+
+        public void GetPublicKeyString()
+        {
+            //var rsa = new RSACryptoServiceProvider(2048);
+            var rsaKeyPair = DotNetUtilities.GetRsaKeyPair(rsa);
+            var writer = new StringWriter();
+            var pemWriter = new PemWriter(writer);
+            pemWriter.WriteObject(rsaKeyPair.Public);
+            pemWriter.WriteObject(rsaKeyPair.Private);
+            string path = @"E:\Visual_Studio_Projects\Information_security_03\key.txt";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(writer);
+                }
+            }
+            /*var lines = System.IO.File.ReadAllLines(@"E:\Visual_Studio_Projects\Information_systems_02\key.txt");
+            System.IO.File.WriteAllLines(@"E:\Visual_Studio_Projects\Information_systems_02\key.txt", lines.Skip(1).ToArray());
+            lines = System.IO.File.ReadAllLines(@"E:\Visual_Studio_Projects\Information_systems_02\key.txt");
+            System.IO.File.WriteAllLines(@"E:\Visual_Studio_Projects\Information_systems_02\key.txt", lines.Take(lines.Length - 2).ToArray());*/
+        }
+    }
+}
+
